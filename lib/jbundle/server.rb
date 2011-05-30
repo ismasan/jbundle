6bbfb1f379f4c437a076b06de26367cb9684969c
Ismael Celis <ismaelct@gmail.com>
2011-05-30 17:07:01 +0100

    Server: Better error message when undefined bundle

--- lib/jbundle/server.rb
+++ lib/jbundle/server.rb
@@ -10,12 +10,13 @@ module JBundle
     # Configure JBundle on every request. 
     # Expensive but allows for reloading changes to JFile
     def call(env)
+      bundle_name = env['PATH_INFO'].sub('/', '')
       begin
         JBundle.config_from_file(@jfile)
-        bundle_name = env['PATH_INFO'].sub('/', '')
         [200, {'Content-Type' => 'application/x-javascript'}, [JBundle.build(bundle_name).src]]
       rescue NoBundleError => boom
-        [404, {'Content-Type' => 'text/plain'}, ["No bundle defined. Try /[bundle_name].js as per your JFile"]]
+        p = bundle_name == '' ? '[bundle_name].js' : bundle_name
+        [404, {'Content-Type' => 'text/plain'}, ["No bundle defined. Try defining /#{p} in your JFile"]]
       rescue NoJFileError => boom
         [404, {'Content-Type' => 'text/plain'}, [boom.message]]
       end
